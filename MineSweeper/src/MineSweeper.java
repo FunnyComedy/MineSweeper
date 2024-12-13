5db5a1e24f0035626e71c32af8c51a2c34a09b68
Hosiah Hedima <hhedima@guilford.edu>
2024-12-13 01:21:15 -0500

    Game loop

--- MineSweeper/src/MineSweeper.java
+++ MineSweeper/src/MineSweeper.java
@@ -4,22 +4,28 @@ import javax.swing.border.LineBorder;
 import java.lang.*;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class MineSweeper extends JFrame{
     // field
     private int rows = 16;
     private int cols = 30;
     private JPanel fieldPanel = new JPanel(new GridLayout(rows, cols));
-    GridBagConstraints nConstraints = new GridBagConstraints();
+    private ImageIcon emptyIcon, oneIcon, twoIcon, threeIcon, fourIcon, fiveIcon, sixIcon, sevenIcon, eightIcon;
     
+    // emptyIcon = new ImageIcon(getClass().getResource("/empty.png"));
+
 
     // info
-    private int time = 43210;
-    
+    private static int time = 0;
+    private static int ticks = 0;
+
+    private static int mine = 99;
 
     private JPanel infoPanel = new JPanel(new BorderLayout());
-    private JLabel timeLabel = new JLabel("56789" + time);
-    private JLabel countLabel = new JLabel();
+    private static JLabel timeLabel = new JLabel("Seconds: " + time);
+    private static JLabel mineLabel = new JLabel("Mines: " + mine);
 
 
 
@@ -31,9 +37,12 @@ public class MineSweeper extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
+
         // field
         for (int i = 0; i < rows * cols; i++) {
-            fieldPanel.add(new Square());
+            Square square = new Square();
+            square.addMouseListener(square);
+            fieldPanel.add(square);
         }
 
         add(fieldPanel, BorderLayout.SOUTH);
@@ -42,28 +51,49 @@ public class MineSweeper extends JFrame{
         // info
         infoPanel.setBackground(Color.BLACK);
 
-        countLabel.setForeground(Color.RED);
-        countLabel.setFont(new Font("Consolas", Font.PLAIN, 32));
+        mineLabel.setForeground(Color.RED);
+        mineLabel.setFont(new Font("Consolas", Font.PLAIN, 32));
 
         timeLabel.setForeground(Color.RED);
         timeLabel.setFont(new Font("Consolas", Font.PLAIN, 32));
 
-        infoPanel.add(countLabel, BorderLayout.WEST);
+        infoPanel.add(mineLabel, BorderLayout.WEST);
         infoPanel.add(timeLabel, BorderLayout.EAST);
 
         add(infoPanel, BorderLayout.NORTH);
     }
 
     // makes grid
-    public class Square extends JPanel {
+    public class Square extends JButton implements MouseListener {
         public Square() {
             setBackground(Color.lightGray);
             setPreferredSize(new Dimension(32, 32));
             setBorder(new LineBorder(Color.darkGray, 2));
         }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            System.out.println("Pressed!");
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            System.out.println("Released!");
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+        }
         
     } 
-
 
     
 
@@ -72,5 +102,25 @@ public class MineSweeper extends JFrame{
     public static void main(String[] args) {
         MineSweeper k = new MineSweeper();
         k.setVisible(true);
+
+        // game loop
+        while (true) {
+            ticks++;
+
+            if (ticks >= 20) {
+                time++;
+                ticks = 0;
+            }
+            
+            timeLabel.setText("" + time);
+            mineLabel.setText("" + mine);
+
+            
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
